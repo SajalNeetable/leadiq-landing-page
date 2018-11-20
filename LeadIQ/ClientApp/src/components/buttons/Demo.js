bf94babdb465d68655512236c60c4126137ed328
niktha <adhithyaprabhu77@gmail.com>
2018-11-20 22:26:09 +0530

    revert back the changes

--- LeadIQ/ClientApp/src/components/buttons/Demo.js
+++ LeadIQ/ClientApp/src/components/buttons/Demo.js
@@ -3,7 +3,7 @@ class Demo extends React.Component {
     render() {
         return (
             <div>
-                <a href="https://leadiq.chilipiper.com/router/talk-to-sales" className={"mt-2 btn liq-btn-secondary button-design liq-btn-primary-" + this.props.size}>GET A DEMO</a>
+                <a href="https://calendly.com/leadiq/trial-kickoff/" className={"mt-2 btn liq-btn-secondary button-design liq-btn-primary-" + this.props.size}>GET A DEMO</a>
             </div>
         );
     }
