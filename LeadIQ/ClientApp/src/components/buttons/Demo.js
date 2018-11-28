4d78da8a1d0ca2dd3788521780209a50143a68f1
Adhithya Prabhi <adhithya@neetable.com>
2018-11-28 18:45:36 +0530

    lightbox added

--- LeadIQ/ClientApp/src/components/buttons/Demo.js
+++ LeadIQ/ClientApp/src/components/buttons/Demo.js
@@ -1,12 +1,63 @@
 ﻿import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import { withStyles } from '@material-ui/core/styles';
+import DialogContent from '@material-ui/core/DialogContent';
+const styles = {
+    dialogPaper: {
+        minHeight: '50vh',
+        maxHeight: '50vh',
+        maxWidth: '400px',
+        overflow: 'hidden'
+    },
+    MuiDialogContentroot45: {
+        padding: 0
+    }
+}
+
 class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 0,
+            open: false
+        };
+    }
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
+
+    getDemo() {
+
+    }
     render() {
+        const { fullScreen, classes } = this.props;
         return (
             <div>
-                <a href="https://calendly.com/leadiq/trial-kickoff/" className={"mt-2 btn liq-btn-secondary button-design liq-btn-primary-" + this.props.size}>GET A DEMO</a>
+                <div className={"mt-2 btn liq-btn-secondary button-design liq-btn-primary-" + this.props.size}
+                    onClick={this.handleClickOpen.bind(this)}>GET A DEMO</div>
+                <Dialog
+                    fullScreen={fullScreen}
+                    open={this.state.open}
+                    onClose={this.handleClose.bind(this)}
+                    aria-labelledby="responsive-dialog-title"
+                    classes={(this.state.mode === 0) ? "" : { paper: classes.dialogPaper }}
+                >
+                    <DialogContent style={{ padding: "0" }}>
+                        <div className="modal-body" style={{ minWidth: "550px" }}>
+                            <iframe id="frame" src="https://leadiq.chilipiper.com/router/talk-to-sales" className="lightboxDesign" ></iframe>
+                        </div>
+                    </DialogContent>
+                </Dialog>
             </div>
         );
     }
 }
 
-export default Demo;
+export default (withStyles(styles)(Demo));
