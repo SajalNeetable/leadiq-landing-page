bd1ed889147350fdf85994f7946a82aeb4071db1
SajalNeetable <47847607+SajalNeetable@users.noreply.github.com>
2019-02-27 18:05:04 +0530

    Sales Demo and Thank You Success new Pages Created, added script in sales demo page

---
+++ LeadIQ/ClientApp/src/pages/SalesDemo.js
@@ -0,0 +1,83 @@
+import React, { Component, Fragment } from "react";
+import Helmet from 'react-helmet';
+class SalesDemo extends Component {
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = "https://js.chilipiper.com/marketing.js";
+        script.async = true;
+        script.onload = () => this.scriptLoaded();
+
+        document.body.appendChild(script);
+    }
+    scriptLoaded() {
+        
+    }
+    render() {
+        let salesdemoTitle = "LeadIQ for Developers - Data APIs";
+        return (
+            <Fragment>
+                <Helmet>
+                    <title>{salesdemoTitle}</title>
+                    <meta name="description" content={salesdemoTitle} />
+                </Helmet>
+                <div className="App">
+                    <div className="leadiq-sales-demo">
+                        <div className="leadiq-sales-demo-innerbanner">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-sm-6 col-xs-12">
+                                        <div className="leadiq-sales-demo-leftcont">
+                                            <h1>See how Lead IQ works<br /> for your entire sales team </h1>
+                                            <p>Cut through the clutter with LeadlQ. Research-based prospecting is on the rise and your team should be on board. Let us show you how you can use LeadlQ to increase sales pipeline with the right prospects. </p>
+                                            <div className="row">
+                                                <div className="col-lg-4 col-xs-12">
+                                                    <img src="/img/sales-demo/bitmap@3x.jpg" className="img-responsive" alt="" />
+                                                </div>
+                                                <div className="col-lg-8 col-xs-12">
+                                                    <blockquote>
+                                                        “LeadIQ saves us three or four steps per new prospect. This allows us to prospect faster and more efficiently.”
+                                                    </blockquote>
+                                                    <span>Charles Moreton, Head of Sales, Videolicious </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6 col-xs-12">
+                                        <div className="leadiq-sales-demo-requestform">
+                                            <form>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="First Name" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="Last Name" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="Email" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="Phone" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="Company Name" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group">
+                                                    <input placeholder="Employee Count" className="leadiq-sales-demo-form-control" />
+                                                </div>
+                                                <div className="leadiq-sales-demo-from-group leadiq-sales-demo-submit">
+                                                    <a href="">See it in action</a>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </Fragment>
+        );
+    }
+}
+
+export default SalesDemo;
---
+++ LeadIQ/ClientApp/src/pages/ThankYouSuccess.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from "react";
+import Helmet from 'react-helmet';
+class ThankYouSuccess extends Component {
+
+    render() {
+        let ThankYouSuccessTitle = "LeadIQ for Developers - Data APIs";
+        return (
+            <Fragment>
+                <Helmet>
+                    <title>{ThankYouSuccessTitle}</title>
+                    <meta name="description" content={ThankYouSuccessTitle} />
+                </Helmet>
+                <div className="App">
+                    <div className="leadiq-sales-demo">
+                        <div className="leadiq-sales-demo-innerbanner">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col-sm-6 col-xs-12">
+                                        <div className="leadiq-sales-demo-leftcont thank-you-success-content">
+                                            <h1>We look forward to chatting soon!</h1>
+                                            <p>If not in a hurry, you can check out how pumped we are for being named a leader on G2Crowd.</p>
+                                            
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6 col-xs-12">
+                                        <div className="leadiq-sales-demo-requestform leadiq-sales-demo-thankyousuccess">
+                                            <iframe src="https://www.youtube.com/embed/yyDwkkeJu4w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default ThankYouSuccess;
